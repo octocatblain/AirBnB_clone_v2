88a52712d63e8c44b2c990ba0e36eef5981c885c
Blain Muema <blainmasaku2@gmail.com>
2023-12-07 08:15:27 +0000

    Task 3. Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    Fabric script that distributes an archive to your web servers
+Fabric script that distributes an archive to your web servers
 """
 
 from fabric.api import *
